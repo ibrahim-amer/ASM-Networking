6a65695d8e9faf9bfdbd9e8003e3a2d0fb5085f6
Ibrahim Mohammed Ali Amer <ibrahim.amer@live.com>
2018-12-10 01:06:39 +0200

    Add project files.

---
+++ DllProject_template/main.asm
@@ -0,0 +1,13 @@
+include irvine32.inc
+.data
+;Write your data here
+.code
+;Write your procedures here
+
+
+; DllMain is required for any DLL
+DllMain PROC hInstance:DWORD, fdwReason:DWORD, lpReserved:DWORD
+mov eax, 1 ; Return true to caller.
+ret
+DllMain ENDP
+END DllMain
